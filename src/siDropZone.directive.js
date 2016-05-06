00943adce219d33aca97bbdf7f8c7d2c79d5aa3e
Carlos Lawton <blakgeek@gmail.com>
2016-05-06 16:21:45 -0400

    Aliased directives with more meaningful names. Updated examples and readme

---
+++ src/siDropZone.directive.js
@@ -0,0 +1,47 @@
++function () {
+    'use strict';
+
+    angular.module('bg.spreadit').directive("siDropzone", ['$rootScope', directive]);
+    angular.module('bg.spreadit').directive("siImporter", ['$rootScope', directive]);
+
+    function directive($rootScope) {
+        var accepts = [];
+
+        if(!!(window.XLSX && XLSX.utils)) {
+            accepts.push('.xls', '.xlsx');
+        }
+
+        if(!!(window.Papa && Papa.parse)) {
+            accepts.push('.csv', '.tsv', '.txt');
+        }
+
+        return {
+            restrict: 'E',
+            scope: {
+                id: '@siId'
+            },
+            controller: controller,
+            controllerAs: 'vm',
+            templateUrl: '/dropzone.html',
+            link: link
+        };
+
+        function controller() {
+
+        }
+
+        function link($scope, $element, $attrs) {
+
+            _.defaults($scope, {
+                id: ""
+            });
+
+            var element = $element[0];
+            element.setAttribute('accepts', accepts.join());
+            element.querySelector('input[type="file"]').addEventListener('change', function (e) {
+                $rootScope.$emit('si.preview', $scope.id, e.target.files[0]);
+            });
+        }
+    }
+
+}();
